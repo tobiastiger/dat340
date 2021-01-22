e9a015414c2314147a06e75ecc268049ffab8ed6
Tobias Tiger <34452589+tobiastiger@users.noreply.github.com>
2021-01-22 12:51:30 +0100

    Task 4 started

--- vsc/assignment1.py
+++ vsc/assignment1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[1]:
 
 
 import pandas as pd
@@ -17,7 +17,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 
-# In[3]:
+# In[2]:
 
 
 # Read the CSV file.
@@ -45,33 +45,33 @@ Y = data_shuffled['NSP'].apply(to_label)
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 
-# In[13]:
+# In[3]:
 
 
 clf_dummy = DummyClassifier(strategy='most_frequent')
-cross_val_score(clf_dummy, Xtrain, Ytrain)
+cross_val_score(clf_dummy, Xtrain, Ytrain, cv=5)
 
 
-# In[14]:
+# In[4]:
 
 
 clf_tree = DecisionTreeClassifier(random_state=0)
-cross_val_score(clf_tree, Xtrain, Ytrain)
+cross_val_score(clf_tree, Xtrain, Ytrain, cv=5)
 
 
-# In[16]:
+# In[5]:
 
 
 clf_forest = RandomForestClassifier(random_state=0)
-cross_val_score(clf_forest, Xtrain, Ytrain)
+cross_val_score(clf_forest, Xtrain, Ytrain, cv=5)
 
 
-# In[26]:
+# In[6]:
 
 
 clf_SVM = make_pipeline(StandardScaler(),
-                    LinearSVC(random_state=0, tol=1e-5, max_iter=10000))
-cross_val_score(clf_SVM, Xtrain, Ytrain)
+                    LinearSVC(random_state=0, max_iter=10000))
+cross_val_score(clf_SVM, Xtrain, Ytrain, cv=5)
 
 
 # In[29]:
@@ -82,13 +82,360 @@ Yguess = clf_forest.predict(Xtest)
 print(accuracy_score(Ytest, Yguess))
 
 
+# ## Task 2
+
+# In[34]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import Counter
+from graphviz import Digraph
+from sklearn.base import BaseEstimator, ClassifierMixin
+from abc import ABC, abstractmethod
+
+
+# In[20]:
+
+
+def majority_sum_scorer(n_low, low_distr, n_high, high_distr):
+    maj_sum_low = low_distr.most_common(1)[0][1]
+    maj_sum_high = high_distr.most_common(1)[0][1]
+    return maj_sum_low + maj_sum_high
+    
+def entropy(distr):
+    n = sum(distr.values())
+    ps = [n_i/n for n_i in distr.values()]
+    return -sum(p*np.log2(p) if p > 0 else 0 for p in ps)
+
+def info_gain_scorer(n_low, low_distr, n_high, high_distr):
+    return -(n_low*entropy(low_distr)+n_high*entropy(high_distr))/(n_low+n_high)
+
+def gini_impurity(distr):
+    n = sum(distr.values())
+    ps = [n_i/n for n_i in distr.values()]
+    return 1-sum(p**2 for p in ps)
+    
+def gini_scorer(n_low, low_distr, n_high, high_distr):
+    return -(n_low*gini_impurity(low_distr)+n_high*gini_impurity(high_distr))/(n_low+n_high)
+
+
+# In[24]:
+
+
+class DecisionTreeLeaf:
+
+    def __init__(self, value):
+        self.value = value
+
+    # This method computes the prediction for this leaf node. This will just return a constant value.
+    def predict(self, x):
+        return self.value
+
+    # Utility function to draw a tree visually using graphviz.
+    def draw_tree(self, graph, node_counter, names):
+        node_id = str(node_counter)
+        val_str = f'{self.value:.4g}' if isinstance(self.value, float) else str(self.value)
+        graph.node(node_id, val_str, style='filled')
+        return node_counter+1, node_id
+        
+    def __eq__(self, other):
+        if isinstance(other, DecisionTreeLeaf):
+            return self.value == other.value
+        else:
+            return False
+
+
+# In[25]:
+
+
+class DecisionTreeBranch:
+
+    def __init__(self, feature, threshold, low_subtree, high_subtree):
+        self.feature = feature
+        self.threshold = threshold
+        self.low_subtree = low_subtree
+        self.high_subtree = high_subtree
+
+    # For a branch node, we compute the prediction by first considering the feature, and then 
+    # calling the upper or lower subtree, depending on whether the feature is or isn't greater
+    # than the threshold.
+    def predict(self, x):
+        if x[self.feature] <= self.threshold:
+            return self.low_subtree.predict(x)
+        else:
+            return self.high_subtree.predict(x)
+
+    # Utility function to draw a tree visually using graphviz.
+    def draw_tree(self, graph, node_counter, names):
+        node_counter, low_id = self.low_subtree.draw_tree(graph, node_counter, names)
+        node_counter, high_id = self.high_subtree.draw_tree(graph, node_counter, names)
+        node_id = str(node_counter)
+        fname = f'F{self.feature}' if names is None else names[self.feature]
+        lbl = f'{fname} > {self.threshold:.4g}?'
+        graph.node(node_id, lbl, shape='box', fillcolor='yellow', style='filled, rounded')
+        graph.edge(node_id, low_id, 'False')
+        graph.edge(node_id, high_id, 'True')
+        return node_counter+1, node_id
+        
+
+
+# In[21]:
+
+
+class DecisionTree(ABC, BaseEstimator):
+
+    def __init__(self, max_depth):
+        super().__init__()
+        self.max_depth = max_depth
+        
+    # As usual in scikit-learn, the training method is called *fit*. We first process the dataset so that
+    # we're sure that it's represented as a NumPy matrix. Then we call the recursive tree-building method
+    # called make_tree (see below).
+    def fit(self, X, Y):
+        if isinstance(X, pd.DataFrame):
+            self.names = X.columns
+            X = X.to_numpy()
+        elif isinstance(X, list):
+            self.names = None
+            X = np.array(X)
+        else:
+            self.names = None
+        Y = np.array(Y)        
+        self.root = self.make_tree(X, Y, self.max_depth)
+        
+    def draw_tree(self):
+        graph = Digraph()
+        self.root.draw_tree(graph, 0, self.names)
+        return graph
+    
+    # By scikit-learn convention, the method *predict* computes the classification or regression output
+    # for a set of instances.
+    # To implement it, we call a separate method that carries out the prediction for one instance.
+    def predict(self, X):
+        if isinstance(X, pd.DataFrame):
+            X = X.to_numpy()
+        return [self.predict_one(x) for x in X]
+
+    # Predicting the output for one instance.
+    def predict_one(self, x):
+        return self.root.predict(x)        
+
+    # This is the recursive training 
+    def make_tree(self, X, Y, max_depth):
+
+        # We start by computing the default value that will be used if we'll return a leaf node.
+        # For classifiers, this will be the most common value in Y.
+        default_value = self.get_default_value(Y)
+
+        # First the two base cases in the recursion: is the training set completely
+        # homogeneous, or have we reached the maximum depth? Then we need to return a leaf.
+
+        # If we have reached the maximum depth, return a leaf with the majority value.
+        if max_depth == 0:
+            return DecisionTreeLeaf(default_value)
+
+        # If all the instances in the remaining training set have the same output value,
+        # return a leaf with this value.
+        if self.is_homogeneous(Y):
+            return DecisionTreeLeaf(default_value)
+
+        # Select the "most useful" feature and split threshold. To rank the "usefulness" of features,
+        # we use one of the classification or regression criteria.
+        # For each feature, we call best_split (defined in a subclass). We then maximize over the features.
+        n_features = X.shape[1]
+        _, best_feature, best_threshold = max(self.best_split(X, Y, feature) for feature in range(n_features))
+        
+        if best_feature is None:
+            return DecisionTreeLeaf(default_value)
+
+        # Split the training set into subgroups, based on whether the selected feature is greater than
+        # the threshold or not
+        X_low, X_high, Y_low, Y_high = self.split_by_feature(X, Y, best_feature, best_threshold)
+
+        # Build the subtrees using a recursive call. Each subtree is associated
+        # with a value of the feature.
+        low_subtree = self.make_tree(X_low, Y_low, max_depth-1)
+        high_subtree = self.make_tree(X_high, Y_high, max_depth-1)
+
+        if low_subtree == high_subtree:
+            return low_subtree
+
+        # Return a decision tree branch containing the result.
+        return DecisionTreeBranch(best_feature, best_threshold, low_subtree, high_subtree)
+    
+    # Utility method that splits the data into the "upper" and "lower" part, based on a feature
+    # and a threshold.
+    def split_by_feature(self, X, Y, feature, threshold):
+        low = X[:,feature] <= threshold
+        high = ~low
+        return X[low], X[high], Y[low], Y[high]
+    
+    # The following three methods need to be implemented by the classification and regression subclasses.
+    
+    @abstractmethod
+    def get_default_value(self, Y):
+        pass
+
+    @abstractmethod
+    def is_homogeneous(self, Y):
+        pass
+
+    @abstractmethod
+    def best_split(self, X, Y, feature):
+        pass
+
+
+# In[22]:
+
+
+class TreeClassifier(DecisionTree, ClassifierMixin):
+
+    def __init__(self, max_depth=10, criterion='maj_sum'):
+        super().__init__(max_depth)
+        self.criterion = criterion
+        
+    def fit(self, X, Y):
+        # For decision tree classifiers, there are some different ways to measure
+        # the homogeneity of subsets.
+        if self.criterion == 'maj_sum':
+            self.criterion_function = majority_sum_scorer
+        elif self.criterion == 'info_gain':
+            self.criterion_function = info_gain_scorer
+        elif self.criterion == 'gini':
+            self.criterion_function = gini_scorer
+        else:
+            raise Exception(f'Unknown criterion: {self.criterion}')
+        super().fit(X, Y)
+        self.classes_ = sorted(set(Y))
+
+    # Select a default value that is going to be used if we decide to make a leaf.
+    # We will select the most common value.
+    def get_default_value(self, Y):
+        self.class_distribution = Counter(Y)
+        return self.class_distribution.most_common(1)[0][0]
+    
+    # Checks whether a set of output values is homogeneous. In the classification case, 
+    # this means that all output values are identical.
+    # We assume that we called get_default_value just before, so that we can access
+    # the class_distribution attribute. If the class distribution contains just one item,
+    # this means that the set is homogeneous.
+    def is_homogeneous(self, Y):
+        return len(self.class_distribution) == 1
+        
+    # Finds the best splitting point for a given feature. We'll keep frequency tables (Counters)
+    # for the upper and lower parts, and then compute the impurity criterion using these tables.
+    # In the end, we return a triple consisting of
+    # - the best score we found, according to the criterion we're using
+    # - the id of the feature
+    # - the threshold for the best split
+    def best_split(self, X, Y, feature):
+
+        # Create a list of input-output pairs, where we have sorted
+        # in ascending order by the input feature we're considering.
+        sorted_indices = np.argsort(X[:, feature])        
+        X_sorted = list(X[sorted_indices, feature])
+        Y_sorted = list(Y[sorted_indices])
+
+        n = len(Y)
+
+        # The frequency tables corresponding to the parts *before and including*
+        # and *after* the current element.
+        low_distr = Counter()
+        high_distr = Counter(Y)
+
+        # Keep track of the best result we've seen so far.
+        max_score = -np.inf
+        max_i = None
+
+        # Go through all the positions (excluding the last position).
+        for i in range(0, n-1):
+
+            # Input and output at the current position.
+            x_i = X_sorted[i]
+            y_i = Y_sorted[i]
+            
+            # Update the frequency tables.
+            low_distr[y_i] += 1
+            high_distr[y_i] -= 1
+
+            # If the input is equal to the input at the next position, we will
+            # not consider a split here.
+            #x_next = XY[i+1][0]
+            x_next = X_sorted[i+1]
+            if x_i == x_next:
+                continue
+
+            # Compute the homogeneity criterion for a split at this position.
+            score = self.criterion_function(i+1, low_distr, n-i-1, high_distr)
+
+            # If this is the best split, remember it.
+            if score > max_score:
+                max_score = score
+                max_i = i
+
+        # If we didn't find any split (meaning that all inputs are identical), return
+        # a dummy value.
+        if max_i is None:
+            return -np.inf, None, None
+
+        # Otherwise, return the best split we found and its score.
+        split_point = 0.5*(X_sorted[max_i] + X_sorted[max_i+1])
+        return max_score, feature, split_point
+
+
+# In[30]:
+
+
+N = 11
+result = np.empty((0,2), int)
+for md in range(1, N):
+    cls = TreeClassifier(max_depth=md)
+    cv_score = cross_val_score(cls, Xtrain, Ytrain, cv=5)
+    tmp = np.array([[md, np.mean(cv_score)]])
+    result = np.vstack((result, tmp))
+
+
+# In[44]:
+
+
+plt.plot(result[:,0], 1-result[:,1])
+plt.show()
+
+
+# In[53]:
+
+
+cls = TreeClassifier(max_depth=4)
+cls.fit(Xtrain, Ytrain)
+Yguess = cls.predict(Xtest)
+print(accuracy_score(Ytest, Yguess))
+
+
+# In[49]:
+
+
+cls = TreeClassifier(max_depth=3)
+cls.fit(Xtrain, Ytrain)
+cls.draw_tree()
+
+
 # ## Task 3
 
-# In[ ]:
+# In[63]:
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import mean_squared_error
+
+
+# In[55]:
 
 
 # Read the CSV file using Pandas.
-alldata = pd.read_csv(LOCATION_OF_YOUR_FILE)
+alldata = pd.read_csv('data/sberbank.csv')
 
 # Convert the timestamp string to an integer representing the year.
 def get_year(timestamp):
@@ -112,16 +459,123 @@ Y = alldata_shuffled['price_doc'].apply(np.log)
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 
-# In[ ]:
+# In[95]:
 
 
-m1 = DummyRegressor()
+m1 = RandomForestRegressor(n_estimators=5, max_depth=10)
 cross_validate(m1, Xtrain, Ytrain, scoring='neg_mean_squared_error')
 
 
+# In[88]:
+
+
+m1.fit(Xtrain, Ytrain)
+mean_squared_error(Ytest, m1.predict(Xtest))
+
+
+# ## Task 4
+
+# In[97]:
+
+
+from sklearn.base import RegressorMixin
+
+
 # In[ ]:
 
 
-regr.fit(Xtrain, Ytrain)
-mean_squared_error(Ytest, regr.predict(Xtest))
+class TreeClassifierRegr(DecisionTree, RegressorMixin):
+
+    def __init__(self, max_depth=10, criterion='maj_sum'):
+        super().__init__(max_depth)
+        self.criterion = criterion
+        
+    def fit(self, X, Y):
+        # For decision tree classifiers, there are some different ways to measure
+        # the homogeneity of subsets.
+        if self.criterion == 'maj_sum':
+            self.criterion_function = majority_sum_scorer
+        elif self.criterion == 'info_gain':
+            self.criterion_function = info_gain_scorer
+        elif self.criterion == 'gini':
+            self.criterion_function = gini_scorer
+        else:
+            raise Exception(f'Unknown criterion: {self.criterion}')
+        super().fit(X, Y)
+        self.classes_ = sorted(set(Y))
+
+    # Select a default value that is going to be used if we decide to make a leaf.
+    # We will select the most common value.
+    def get_default_value(self, Y):
+        self.class_distribution = Counter(Y)
+        return self.class_distribution.most_common(1)[0][0]
+    
+    # Checks whether a set of output values is homogeneous. In the classification case, 
+    # this means that all output values are identical.
+    # We assume that we called get_default_value just before, so that we can access
+    # the class_distribution attribute. If the class distribution contains just one item,
+    # this means that the set is homogeneous.
+    def is_homogeneous(self, Y):
+        var = np.var(class_distribution)
+        return len(self.class_distribution) == 1
+        
+    # Finds the best splitting point for a given feature. We'll keep frequency tables (Counters)
+    # for the upper and lower parts, and then compute the impurity criterion using these tables.
+    # In the end, we return a triple consisting of
+    # - the best score we found, according to the criterion we're using
+    # - the id of the feature
+    # - the threshold for the best split
+    def best_split(self, X, Y, feature):
+
+        # Create a list of input-output pairs, where we have sorted
+        # in ascending order by the input feature we're considering.
+        sorted_indices = np.argsort(X[:, feature])        
+        X_sorted = list(X[sorted_indices, feature])
+        Y_sorted = list(Y[sorted_indices])
+
+        n = len(Y)
+
+        # The frequency tables corresponding to the parts *before and including*
+        # and *after* the current element.
+        low_distr = Counter()
+        high_distr = Counter(Y)
+
+        # Keep track of the best result we've seen so far.
+        max_score = -np.inf
+        max_i = None
+
+        # Go through all the positions (excluding the last position).
+        for i in range(0, n-1):
+
+            # Input and output at the current position.
+            x_i = X_sorted[i]
+            y_i = Y_sorted[i]
+            
+            # Update the frequency tables.
+            low_distr[y_i] += 1
+            high_distr[y_i] -= 1
+
+            # If the input is equal to the input at the next position, we will
+            # not consider a split here.
+            #x_next = XY[i+1][0]
+            x_next = X_sorted[i+1]
+            if x_i == x_next:
+                continue
+
+            # Compute the homogeneity criterion for a split at this position.
+            score = self.criterion_function(i+1, low_distr, n-i-1, high_distr)
+
+            # If this is the best split, remember it.
+            if score > max_score:
+                max_score = score
+                max_i = i
+
+        # If we didn't find any split (meaning that all inputs are identical), return
+        # a dummy value.
+        if max_i is None:
+            return -np.inf, None, None
+
+        # Otherwise, return the best split we found and its score.
+        split_point = 0.5*(X_sorted[max_i] + X_sorted[max_i+1])
+        return max_score, feature, split_point
 

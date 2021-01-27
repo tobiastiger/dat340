f14aa2bd978a40e3a68defc296a38f4385caea78
Tobias Tiger <34452589+tobiastiger@users.noreply.github.com>
2021-01-27 09:02:57 +0100

    Continue assignment 2

--- vsc/assignment2.py
+++ vsc/assignment2.py
@@ -5,13 +5,13 @@
 
 # ## Step 1
 
-# In[2]:
+# In[1]:
 
 
 import pandas as pd
 
 
-# In[8]:
+# In[2]:
 
 
 adult_train = pd.read_csv('data/adult_train.csv')
@@ -26,7 +26,7 @@ Ytest = adult_test['target']
 
 # ## Step 2
 
-# In[32]:
+# In[3]:
 
 
 from sklearn.feature_extraction import DictVectorizer
@@ -35,7 +35,7 @@ from sklearn.model_selection import cross_validate
 from sklearn.metrics import accuracy_score
 
 
-# In[45]:
+# In[4]:
 
 
 dicts_training_data = Xtrain.to_dict('records')
@@ -46,7 +46,7 @@ X_train_encoded = dv.fit_transform(dicts_training_data)
 X_test_encoded = dv.transform(dicts_testing_data)
 
 
-# In[31]:
+# In[5]:
 
 
 clf_forest = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
@@ -54,20 +54,20 @@ clf_forest = RandomForestClassifier(n_estimators=100, max_depth=10, random_state
 cv_result = cross_validate(clf_forest, X_train_encoded, Ytrain, cv=5, scoring='accuracy')
 
 
-# In[35]:
+# In[6]:
 
 
 print(cv_result)
 
 
-# In[46]:
+# In[7]:
 
 
 clf_forest.fit(X_train_encoded, Ytrain)
 predictions = clf_forest.predict(X_test_encoded)
 
 
-# In[49]:
+# In[8]:
 
 
 accuracy_score(Ytest, predictions)
@@ -75,13 +75,20 @@ accuracy_score(Ytest, predictions)
 
 # ## Step 3
 
-# In[50]:
+# In[20]:
 
 
 from sklearn.pipeline import make_pipeline
 
 
-# In[58]:
+# In[25]:
+
+
+Xtrain_dict = Xtrain.to_dict('records')
+Xtest_dict = Xtest.to_dict('records')
+
+
+# In[30]:
 
 
 pipeline = make_pipeline(
@@ -90,15 +97,83 @@ pipeline = make_pipeline(
 )
 
 
-# In[59]:
+# In[32]:
 
 
-pipeline
+pipeline.fit(Xtrain_dict, Ytrain)
+accuracy_score(Ytest, pipeline.predict(Xtest_dict))
 
 
-# In[60]:
+# # Task 2
+
+# ## DecisionTreeClassifier
+
+# In[45]:
 
 
-pipeline.fit(Xtrain, Ytrain)
-accuracy_score(Ytest, pipeline.predict(Xtest))
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+
+# In[113]:
+
+
+N = 21
+results = np.empty((0, 4), int)
+for md in range(1, N):
+    clf_tree = DecisionTreeClassifier(max_depth=md, random_state=0)
+    clf_tree.fit(X_train_encoded, Ytrain)
+    predictions = clf_tree.predict(X_test_encoded)
+    accuracy = accuracy_score(Ytest, predictions)
+    precision = precision_score(Ytest, predictions, pos_label='<=50K')
+    recall = recall_score(Ytest, predictions, pos_label='<=50K')
+    results = np.append(results, np.array([[md, accuracy, precision, recall]]), axis=0)
+
+
+# In[114]:
+
+
+print(results)
+
+
+# In[50]:
+
+
+plt.plot(results[:,0], results[:,1])
+plt.show()
+
+
+# ## RandomForestClassifier
+
+# In[151]:
+
+
+N_TREES = [1, 50, 100, 150]
+MD = 5
+results_forest = np.zeros((MD, 4, len(N_TREES)))
+i = 0
+j = 0
+for n_trees in N_TREES:
+    for md in range(1, MD + 1):
+        clf_tree = DecisionTreeClassifier(max_depth=md, random_state=0)
+        clf_tree.fit(X_train_encoded, Ytrain)
+        predictions = clf_tree.predict(X_test_encoded)
+        accuracy = accuracy_score(Ytest, predictions)
+        precision = precision_score(Ytest, predictions, pos_label='<=50K')
+        recall = recall_score(Ytest, predictions, pos_label='<=50K')
+        results_forest[i, :, j] = [md, accuracy, precision, recall]
+        j += 1
+    i += 1
+    j = 0
+
+
+# In[149]:
+
+
+plt.plot(results_forest[0, 0, :], results_forest[0, 1, :])
+plt.show()
 
